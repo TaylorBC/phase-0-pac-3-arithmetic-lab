633a05a420fcc9e3bb37c72cddc9b81001ecdd76
Brian Taylor <taylor.brian99@yahoo.com>
2021-03-14 18:26:04 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(num1, num2) {
+return num1 + num2;
+} 
+ 
+function subtract(num6, num3) {
+    return num6 - num3;
+} 
+
+function multiply(num5, num4) {
+    return num5 * num4;
+}
+
+function divide(num15, num3) {
+    return num15 / num3;
+} 
+
+function increment(n) {
+    return n+1; 
+} 
+
+function decrement(a) {
+    return a-1;
+}
+function makeInt(a) {
+    return parseInt (a, 10);
+} 
+function preserveDecimal(n) {
+return parseFloat(n,2.245); 
+} 
